bde678e68493d4ed59ea82245891cd69c0e30365
Amanuel Getachew <agetachew73@gmail.com>
2022-01-20 22:52:49 +0300

    added project files

---
+++ Clinic_Patient_Info_Management_System/Clinic_Patient_Info_Management_System/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Clinic_Patient_Info_Management_System.MvcApplication" Language="C#" %>
---
+++ Clinic_Patient_Info_Management_System/Clinic_Patient_Info_Management_System/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Clinic_Patient_Info_Management_System.Startup))]
+namespace Clinic_Patient_Info_Management_System
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
