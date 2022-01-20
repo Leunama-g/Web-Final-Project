a0d3ce0a2bc0268115a55c7557cb17bdd1b328e7
Amanuel Getachew <agetachew73@gmail.com>
2022-01-20 15:28:15 +0300

    Project Created

---
+++ ClinicCustomerInfoManagementSystem/ClinicCustomerInfoManagementSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ClinicCustomerInfoManagementSystem.Startup))]
+namespace ClinicCustomerInfoManagementSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
