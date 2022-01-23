898157ee64b0e98181047c44f2ed8e61c25e56b5
Amanuel Getachew <agetachew73@gmail.com>
2022-01-23 12:57:12 +0300

    patient view and domain models done

---
+++ Clinic_Patient_Info_Management_System/Clinic_Patient_Info_Management_System/Models/ReceptionistViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Clinic_Patient_Info_Management_System.Models
+{
+    public class QueueViewModel
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Status { get; set; }
+
+        public string Priority { get; set; }
+    }
+
+    public class ReceptionistViewModel
+    {
+        public List<QueueViewModel> ForPediatrician;
+        public List<QueueViewModel> ForGeriatricians;
+
+
+        public ReceptionistViewModel()
+        {
+            ForPediatrician = new List<QueueViewModel>();
+            ForGeriatricians = new List<QueueViewModel>();
+        }
+    }
+}
